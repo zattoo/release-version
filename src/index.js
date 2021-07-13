3cc717f804e82a57c1ce4a0e18d64b0e2d722229
godban <bogdan.plieshka@zattoo.com>
2021-07-13 17:12:02 +0200

    ...

--- src/index.js
+++ src/index.js
@@ -5,16 +5,21 @@ const github = require('@actions/github');
     // extract input
     const token = core.getInput('token', {required: true});
     const octokit = github.getOctokit(token);
+    const {
+        ref,
+        payload
+    } = github.context;
+    const {repository} = payload;
 
-    const repo = github.context.payload.repository.name;
-    const owner = github.context.payload.repository.full_name.split('/')[0];
+    const repo = repository.name;
+    const owner = repository.full_name.split('/')[0];
 
-    console.log('github.context.sha', github.context.sha);
+    console.log('github.context.ref', ref);
 
     const files = await octokit.repos.getCommit({
         owner,
         repo,
-        ref: github.context.sha,
+        ref,
     });
 
     files.data.map((file) => {
