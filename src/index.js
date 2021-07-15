b6efe9f0c3fb0938a8b194f290caa5687ae5bfaf
Bogdan Plieshka <bogdanplieshka@gmail.com>
2021-07-15 12:52:48 +0200

    ...

--- src/index.js
+++ src/index.js
@@ -135,22 +135,25 @@ const getNewVersions = (changelogBefore, changelogAfter) => {
                 core.info(`Release ${releaseBranch} already exist.\nSee ${releaseUrl}`);
             }
         } else {
+            await exec(`git checkout -b ${releaseBranch}`);
+
+            // await exec(`git checkout ${patchBranch}`);
+
+
             // get release branch
-            const {data: release} = await octokit.rest.git.getRef({
-                owner,
-                repo,
-                ref: `heads/${releaseBranch}`,
-            });
-
-            // branch patch from release
-            await octokit.rest.git.createRef({
-                owner,
-                repo,
-                ref: `refs/heads/${patchBranch}`,
-                sha: release.object.sha,
-            });
-
-            await exec(`git checkout ${patchBranch}`);
+            // const {data: release} = await octokit.rest.git.getRef({
+            //     owner,
+            //     repo,
+            //     ref: `heads/${releaseBranch}`,
+            // });
+            //
+            // // branch patch from release
+            // await octokit.rest.git.createRef({
+            //     owner,
+            //     repo,
+            //     ref: `refs/heads/${patchBranch}`,
+            //     sha: release.object.sha,
+            // });
 
             // get commit to cherry pick
             // const {data: commit} = await octokit.rest.git.getCommit({
