ccb7634e824bb9eac67331be34c1597e99d3e9ca
Bogdan Plieshka <bogdanplieshka@gmail.com>
2021-07-15 09:57:41 +0200

    ...

---
+++ .tmp/response-updateRef.js
@@ -0,0 +1,42 @@
+const response = {
+    status: 200,
+    url: 'https://api.github.com/repos/zattoo/cactus/git/refs/heads%2Fpatch%2Fapp%2F3.1111.4',
+    headers: {
+        'access-control-allow-origin': '*',
+        'access-control-expose-headers': 'ETag, Link, Location, Retry-After, X-GitHub-OTP, X-RateLimit-Limit, X-RateLimit-Remaining, X-RateLimit-Used, X-RateLimit-Resource, X-RateLimit-Reset, X-OAuth-Scopes, X-Accepted-OAuth-Scopes, X-Poll-Interval, X-GitHub-Media-Type, Deprecation, Sunset',
+        'cache-control': 'private, max-age=60, s-maxage=60',
+        connection: 'close',
+        'content-encoding': 'gzip',
+        'content-security-policy': "default-src 'none'",
+        'content-type': 'application/json; charset=utf-8',
+        date: 'Thu, 15 Jul 2021 07:55:36 GMT',
+        etag: 'W/"a0fbddad37afe65860bd3b814a4eee79d7ec6ae09c72f79ae185ecfe807cd815"',
+        'referrer-policy': 'origin-when-cross-origin, strict-origin-when-cross-origin',
+        server: 'GitHub.com',
+        'strict-transport-security': 'max-age=31536000; includeSubdomains; preload',
+        'transfer-encoding': 'chunked',
+        vary: 'Accept, Authorization, Cookie, X-GitHub-OTP, Accept-Encoding, Accept, X-Requested-With',
+        'x-accepted-oauth-scopes': 'repo',
+        'x-content-type-options': 'nosniff',
+        'x-frame-options': 'deny',
+        'x-github-media-type': 'github.v3; format=json',
+        'x-github-request-id': '07C7:1E7F:C48E2B:188E221:60EFE9F7',
+        'x-oauth-scopes': 'delete:packages, gist, notifications, repo, user, workflow, write:discussion, write:packages',
+        'x-ratelimit-limit': '5000',
+        'x-ratelimit-remaining': '4874',
+        'x-ratelimit-reset': '1626337066',
+        'x-ratelimit-resource': 'core',
+        'x-ratelimit-used': '126',
+        'x-xss-protection': '0'
+    },
+    data: {
+        ref: 'refs/heads/patch/app/3.1111.4',
+        node_id: 'MDM6UmVmMzg1NTkyNjE0OnJlZnMvaGVhZHMvcGF0Y2gvYXBwLzMuMTExMS40',
+        url: 'https://api.github.com/repos/zattoo/cactus/git/refs/heads/patch/app/3.1111.4',
+        object: {
+            sha: '9f94046b3fc5e0a250a59377d89b3157b83abd6e',
+            type: 'commit',
+            url: 'https://api.github.com/repos/zattoo/cactus/git/commits/9f94046b3fc5e0a250a59377d89b3157b83abd6e'
+        }
+    }
+};
